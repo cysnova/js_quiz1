4071c51570c73d53e3fd966f7ff87ab86fc29c40
cysnova <cysnova@gmail.com>
2023-01-13 00:21:47 -0800

    first commit

---
+++ quiz.js
@@ -0,0 +1,78 @@
+// 1. ans= Trigger
+
+
+2.
+function secondIndexOf(s1, s2) {
+    let firstIndex = s1.indexOf(s2);
+    return s1.indexOf(s2, firstIndex + 1);
+  }
+
+let a = 100;
+let b = 200;
+
+3.
+function equals(a,b){
+if(a===b){
+  return "EQUAL";
+  return "UNEQUAL";}
+}
+
+console.log(equals(100,1000));
+
+4.
+let age = 10;
+
+if(age>18){
+  console.log("Old enough");
+}else{
+  console.log("Too young");
+}
+
+5.
+function redigit(num,b){
+let prev = -1;
+while (num != 0) {
+      let digit = num % b;
+      num = parseInt(num/b);
+      if (prev != -1 && digit != prev)
+          return 'No Repdigit!';
+      prev = digit;
+  }
+  return 'Repdigit!';
+}
+
+6.
+function unequal(a,b,c){
+  if (a!==b && b!==c && a!==c){
+    return "true";
+  }else{
+    return "false"
+  }
+}
+
+console.log(unequal(1,2,3));
+console.log(unequal(1,0,2));
+
+
+// 7. The first and third alerts are going to execute except the second one which has the result of undefined.
+
+8.
+let userName = prompt("Who's there?", '');
+
+if (userName === 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass === 'TheMaster') {
+    alert( 'Welcome!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'Canceled' );
+  } else {
+    alert( 'Wrong password' );
+  }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}
